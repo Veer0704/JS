dc0fdf21b9feda94d71c4ad93ffba06779c44142
Varun <varunpandya0704@gmail.com>
2024-08-25 15:15:27 +0000

    datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+
+console.log(3+6); console.log("Veer") //code readability should be high
+
+
+let name= "Veer" //string
+let age= 21 //num
+let isLoggedIn = true //boolean
+let state; 
+ 
+//number => 2 to power 53 range of memory 
+//bigint used for large numbers 
+//string => ""
+//boolean => either true or false 
+//null => standalone value 
+//undefined => state in this case is undefined 
+//symbol => unique values 
+
+
+//object 
+console.log(typeof "ABC");
+console.log(typeof null);
+console.log(typeof undefined);
