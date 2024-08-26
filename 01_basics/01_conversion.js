6c2fc128eaef3dee97d3893252e7a43832accf3f
Varun <varunpandya0704@gmail.com>
2024-08-26 07:25:05 +0000

    string to number

--- 01_basics/01_conversion.js
+++ 01_basics/01_conversion.js
@@ -18,3 +18,42 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log( typeof stringNumber);
+
+// ****************** Operations ****************** //
+
+let value = 3 
+let negValue = - value
+console.log(negValue);
+
+let str1 = "hello"
+let str2= " hitesh"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+2+"2"+3); 
+console.log(3+4*5%3); //use parenthesis this is a messy code
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let x = 100
+const y = ++x //here x is 100 and y is 101
+const z = x++ //here x is 100 and z is 100
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
