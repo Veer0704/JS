28fea375c156510b0d6ae18758e8cfd90d898218
Varun <varunpandya0704@gmail.com>
2024-08-27 15:10:40 +0000

    array2 methods

---
+++ 01_basics/01_array2.js
@@ -0,0 +1,12 @@
+const heroes = ["thor", "ironman", "spiderman"]
+const dc_heroes = ["Superman", "batman", "flash"]
+
+heroes.push(dc_heroes)
+
+
+
+const allheroes = heroes.concat(dc_heroes)
+console.log(allheroes);
+
+
+
