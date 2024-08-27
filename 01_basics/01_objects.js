9a0e8d2f5a4b2dfccd7a680ca3238008d09054f8
Varun <varunpandya0704@gmail.com>
2024-08-27 15:24:12 +0000

    objects basic

---
+++ 01_basics/01_objects.js
@@ -0,0 +1,22 @@
+//singleton
+//object literals
+const mySym =  Symbol("key1")
+
+const JsUser ={
+    name: "Veer",
+    [mySym]: "mykey1",
+    age: 20,
+    location: "Jaipur",
+    email: "abc@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Tuesday"]
+} 
+
+console.log(JsUser.email);
+console.log(JsUser["email"]);
+console.log(JsUser[mySym]);
+console.log(typeof mySym);
+
+Object.freeze(JsUser) //this will freezse the object and will not make changes after the freeze // 
+
+
