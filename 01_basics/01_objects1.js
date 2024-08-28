a378b1d743e665fcd8e2281cd677abadb66a4c83
Varun <varunpandya0704@gmail.com>
2024-08-28 12:39:15 +0000

    object assign

--- 01_basics/01_objects1.js
+++ 01_basics/01_objects1.js
@@ -1,12 +1,4 @@
-const tinderUser = new Object()
-const tinderUser = {
 
-    tinderUser.id = "123abc",
-    tinderUser.name = "Veer",
-    tinderUser.isLoggedIn = false
-}
-
-console.log(tinderUser);
 
 const regularUser = {
     email:"abc@gmail.com",
@@ -18,3 +10,16 @@ const regularUser = {
 
 console.log(regularUser.userfullname);
 
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"a", 4:"b"}
+
+const obj3 = { obj1, obj2 }
+const obj4 = Object.assign({}, obj1, obj2) //assign operator is used to copy enumerable objects from one and more sources to target object
+console.log(obj3);
+console.log(obj4); 
+
+
+
+        
+    
