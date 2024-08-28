31083b41c80c90d6ff80dd07a602c64a0f153e1d
Varun Pandya <varunpandya0704@gmail.com>
2024-08-28 18:00:45 +0530

    object methods

---
+++ 01_basics/01_objects1.js
@@ -0,0 +1,20 @@
+const tinderUser = new Object()
+const tinderUser = {
+
+    tinderUser.id = "123abc",
+    tinderUser.name = "Veer",
+    tinderUser.isLoggedIn = false
+}
+
+console.log(tinderUser);
+
+const regularUser = {
+    email:"abc@gmail.com",
+    userfullname:{
+        firstname: "veer",
+        lastname: "p" 
+    }
+}
+
+console.log(regularUser.userfullname);
+
