5a288cdc26133834acfe4c0e95648f88d75195ff
Varun <varunpandya0704@gmail.com>
2024-08-28 16:09:04 +0000

    functions basics

--- 01_basics/01_functions.js
+++ 01_basics/01_functions.js
@@ -13,4 +13,41 @@ function sum(n1,n2){
 const result = sum(3,5)
 console.log("result",result)
 
+function loginUserMessage(username="sam"){
+    if(!username){
+        console.log("Please enter a username");
+        
+    }
+    return `${username} just logged in `
+}
 
+console.log(loginUserMessage());
+
+
+function calculateCartPrice(...num1){
+    return num1
+}
+console.log(calculateCartPrice(200,400,500,2000));
+
+const user = {
+    username:"veer",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+
+handleObject({
+    username:"veer",
+    price:199
+})
+
+const mynewArray = [200,400,100,600]
+function returnSecondValue(getArray){
+
+    return getArray[0]
+}
+
+console.log(returnSecondValue(mynewArray));
