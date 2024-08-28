9c456c32243efa2629e89014ae3919a498e9badb
Varun <varunpandya0704@gmail.com>
2024-08-28 12:48:23 +0000

    object destruction

---
+++ 01_basics/01_objectdestruct.js
@@ -0,0 +1,19 @@
+const course = 
+
+{
+    coursename: "JS",
+    price: "999",
+    instructor: "ht"
+}
+
+//course.instructor
+
+const {instructor} = course ; //object destructured 
+console.log(instructor);
+
+//{
+//    name: "Veer",
+//    coursename: "JS",
+//    cprice: "free"
+//}
+
