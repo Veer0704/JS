ef30d4c43d96e1ca477c835ccd2fcb809b75e89b
Varun <varunpandya0704@gmail.com>
2024-08-28 13:30:46 +0000

    functions

---
+++ 01_basics/01_functions.js
@@ -0,0 +1,16 @@
+function sayMyName() {
+    console.log("V");
+    console.log("e");
+    console.log("e");
+    console.log("r");
+}
+
+sayMyName()
+
+function sum(n1,n2){
+    return n1+n2
+}
+const result = sum(3,5)
+console.log("result",result)
+
+
